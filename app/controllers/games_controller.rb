9b43a009a7841e988b3d60d4183375ec3e62b6fc
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-13 01:09:27 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def create
+    @game = Game.create(game_params)
+    # binding.pry
+  end
+
+  def show
+  end
+  private
+
+  def game_params
+    params.permit(state: [])
+  end
+
 end
